7fa9b63ce3107ac0cb1f8d649247c657378aabc0
Martin Craig <martin.craig@nottingham.ac.uk>
2022-07-25 15:47:43 +0100

    Add hacky code to re-order BVALS when they come wrong out of the converter

--- rodent_anat/preproc.py
+++ rodent_anat/preproc.py
@@ -27,6 +27,7 @@ class ArgumentParser(argparse.ArgumentParser):
         group.add_argument("--noprune", help="Do not remove files that we don't think we need (low-res anatomical, single volume DTI, localizer)", action="store_true", default=False)
         group.add_argument("--noreorient", help="Do not re-orient files to standard orientation", action="store_true", default=False)
         group.add_argument("--nofixdti", help="Do not try to 'fix' S-I flipping in DTI files", action="store_true", default=False)
+        group.add_argument("--nofixbval", help="Do not try to 'fix' BVAL ordering in DTI files", action="store_true", default=False)
         group.add_argument("--nodticpgeom", help="Do not copy the geometry of S-I flipped DTI files from the first non-flipped DTI", action="store_true", default=False)
         group.add_argument("--flipap", help="When re-orienting, flip the A-P axis for all scans. Used when rodent has been put tail-first into the bore rather than nose-first", action="store_true", default=False)
         group.add_argument("--debug", help="Enable debug output", action="store_true", default=False)
@@ -87,3 +88,35 @@ def main():
                     os.system(f'fslcpgeom "{nonflipped_dtis[0]}" "{fpath}" -d')
         else:
             LOG.warn("Can't fix DTI orientation - we don't have an anatomical image")
+
+    # Fix DTI BVAL ordering if requested
+    import numpy as np
+    if not options.nofixbval:
+        for cat, fpaths in categories.items():
+            if cat == DTI:
+                for fpath in fpaths:
+                    try:
+                        bvals = np.loadtxt(sidecar(fpath, "bval"))
+                        bvecs = np.loadtxt(sidecar(fpath, "bvec")).T
+                        zero_bvecs = [idx for idx, bvec in enumerate(bvecs) if np.allclose(bvec, [0, 0, 0])]
+                        num_b0s = len(zero_bvecs)
+                        smallest_bvals = sorted(np.argpartition(bvals, num_b0s)[:num_b0s])
+                        smallest_bvals_copy = list(smallest_bvals)
+                        if zero_bvecs != smallest_bvals:
+                            print("WARNING: Smallest BVALS do not correspond with positions of zero bvecs")
+                            print(f"Zero bvecs at: {zero_bvecs}" % zero_bvecs)
+                            print(f"Smallest bvals at: {smallest_bvals}")
+                            new_bvals, current_bval = [], 0
+                            for idx in range(len(bvals)):
+                                if idx in zero_bvecs:
+                                    new_bvals.append(bvals[smallest_bvals.pop(0)])
+                                else:
+                                    while current_bval in smallest_bvals_copy:
+                                        current_bval += 1
+                                    new_bvals.append(bvals[current_bval])
+                                    current_bval += 1
+                            print("Re-ordered BVALS: %s" % new_bvals)
+                    except:
+                        print(f"WARNING: Failed to get bvals/bvecs from DTI: {fpath}")
+                        import traceback
+                        traceback.print_exc()
